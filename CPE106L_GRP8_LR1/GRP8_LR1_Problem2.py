fdc7acf13c9b85eb4bde9fe8d766e5d9cc189aee
drpdayan <drpdayan@mymail.mapua.edu.ph>
2024-06-22 11:01:38 +0800

    Final LR1

--- CPE106L_GRP8_LR1/GRP8_LR1_Problem2.py
+++ CPE106L_GRP8_LR1/GRP8_LR1_Problem2.py
@@ -1,29 +1,42 @@
 '''
-This program
+This program asks users to open a text file and read it
+Each line is appended with a number according to its positsion
+Users can choose any number that represents any line
+If users choose 0, the program will close 
 '''
+
+#Prompts user to input file name
+#Example Text File : "MapuaAlmaMater.txt"
 txtfile = input("File Name: ")
 
+#Programs opens and reads the text file"
 f = open(txtfile, "r")
 
+
+#Declaration of Variables
 linenum = 0
 chosenlinenum =  0
 append_list = []
-print ("\n" + "Mapua Alma Mater Per Line:" + "\n")
 
+# The program prints the text file alongside its corresponding number per line
+print ("\n" + "Mapua Alma Mater Per Line:" + "\n")
 for line in f:
     linenum += 1
     print("{}: {}".format(linenum,line.strip()))
-    append_list.append("{}: {}".format(linenum,line.strip()))
+    append_list.append("{}: {}".format(linenum,line.strip())) # Each line is then appended on an empty 
+                                                              # list where its index can be used to pick any line
 
 # print(append_list)
 
+#A while loop statement is used to choose a line number
 while True:
     chosenlinenum = int(input("Choose a line number:"))
-    chosenlinenum -= 1
+    chosenlinenum -= 1 #List index starts with 0
     
-    print(append_list[chosenlinenum])
+    
+    print(append_list[chosenlinenum]) #The chosen line number is then displayed 
 
-    if chosenlinenum == 0:
+    if chosenlinenum == 0: #When "0" is chosen, the program exits
         print("Exit.")
         break
 
--- CPE106L_GRP8_LR1/stats.py
+++ CPE106L_GRP8_LR1/stats.py
@@ -1,41 +1,48 @@
-dan = [34, 69, 420, 1337, 420, 23, 80, 190, 2]
+'''
+This python file contains the functions needed 
+For the LR1_Problem1 such as :
+Mean, Median, Mode
+'''
 
+#Calculates the average (mean) on a given list of numbers
 def mean(num):
     mean = 0
-    for i in num:
+    for i in num: #For loops that adds all elements within a list
         mean += i
     mean /= len(num)
-    return mean
+    return mean #Returns mean value
 
 
+#Finds the median on a given list of numbers
 def median(num):
-    num.sort()
+    num.sort() #Sorts the list and finds its middle
     mid = len(num) // 2
     if len(num) % 2 == 1:
         median = (num[mid])
-    else:
-        median = (num[mid] + num[mid - 1] / 2)
-    return median
+    else: #If the list contains an even number of elements
+        median = ((num[mid] + num[mid - 1]) / 2)
+    return median  #Returns median value
 
+
+#Finds the mode on a given list of numbers
 def mode(num):
-    count = []
+    count = [] #Empty list to append each element with its frequency within the list
     for i in num:
         count.append(num.count(i))
     unique_count = []
-    for i in count:
+    for i in count: #Stores unique counts
         if i not in unique_count:
             unique_count.append(i)
-    if len(unique_count) > 1:
-        m = []
+    if len(unique_count) > 1: #Conditional statement if there are multiple or no modes
+        mx = []
         for i in list (range(len(count))):
             if count[i] == max (unique_count):
-                m.append(num[i])
-        mode = []
-        for i in m:
+                mx.append(num[i])
+        mode = [] # Appends potential modes (elements with max frequency)
+        for i in mx:
             if i not in mode:
                 mode.append(i)
-        return mode
-    else:
-        print("No Mode on the List")
+        mode = int(mode[0]) #Transforms the return type from a list to an int
+        return mode #Returns mode value
 
-print(mode(dan))
+
